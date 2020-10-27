14e8aa73ade03c204415e3abfef3b20c577c25cf
Yx_Zou <hello.yxhome@gmail.com>
2020-10-27 14:46:02 +0800

    读取第一篇文章

--- py_youdaoyun/dataList.py
+++ py_youdaoyun/dataList.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+import os
+
+import getContent
 
 id = '62218d1d8f42aea18e84d345e0e6923d'
 
@@ -20,4 +24,14 @@ def refining(data):
             "title": i['tl'],
             "url": purl,
         })
-    print(refiningData)
+    content = getContent.getText(refiningData)
+    content = content.json()
+    htmlContent = BeautifulSoup(content['content'], 'html.parser')
+    htmlContent = htmlContent.prettify()
+    newFile(content['tl'], htmlContent)
+
+
+def newFile(name, content):
+    contentHtml = open(name + '.html', 'w', encoding='utf-8')
+    contentHtml.write(content)
+    contentHtml.close()
