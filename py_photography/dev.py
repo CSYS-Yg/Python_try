69f61a18bbc8135c7323f4052dcb3819012a9513
Yx_Zou <hello.yxhome@gmail.com>
2020-11-10 16:10:36 +0800

    测试本地参数上传

--- py_photography/dev.py
+++ py_photography/dev.py
@@ -1,4 +1,3 @@
-import getContentTest
-
-url = 'https://appt2ioq9ws3997.pc.xiaoe-tech.com/api/xe.goods.relation.get/1.0.0?app_id=appt2iOQ9ws3997'
-getContentTest.postData(url)
+import parameter
+if __name__ == '__main__':
+    print(parameter.cellphone)
